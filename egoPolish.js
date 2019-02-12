81800000a9457be5b903317167c0781571b1d3e8
EnvySun <sunchaoren@163.com>
2019-02-12 16:53:43 +0800

    update

--- egoPolish.js
+++ egoPolish.js
@@ -2,8 +2,8 @@
 // 上回书说道
 // 怎么让使用 双括号进行绑定的数据，在渲染一次后，还可以渲染第二次-----修改了绑定的属性的名称，在发现大括号之后，加一层ego-model
 // 新问题，没有办法控制，没使用标签包裹的绑定点(目前使用检测后强制绑定了一个ego-model，但我觉得这样不太科学)
-// 新问题，如果在一个标签内既有{{}}数据绑定，又有
-// 实现ego-if
+// 新问题，如果在一个标签内既有{{}}数据绑定，又有ego-model该如何处理
+// 新问题，事件绑定，在修改数据之后会重复遍历，这样会将数据绑定四次
 var Observer = /** @class */ (function () {
     function Observer(data, methods, context) {
         var _this = this;
@@ -22,16 +22,15 @@ var Observer = /** @class */ (function () {
         // get和set会将原生的dom操作覆盖掉
         Object.keys(data).forEach(function (key) {
             // self.defineReactive(data, key, data[key])
-            return _this.defineppt(data, key, data[key], context);
+            return _this.defineProp(data, key, data[key], context);
         });
     }
     Observer.prototype.setData = function (dataChange) {
         for (var key in dataChange) {
             this.data[key] = dataChange[key];
         }
-        console.log(this.data);
     };
-    Observer.prototype.defineppt = function (data, key, value, context) {
+    Observer.prototype.defineProp = function (data, key, value, context) {
         // 注意，在defineProperty之中不能直接使用this.data[key]这样的方式，否则会循环调用set和get函数
         Object.defineProperty(data, key, {
             enumerable: false,
@@ -42,6 +41,7 @@ var Observer = /** @class */ (function () {
             },
             set: function (newValue) {
                 // 无论是视图上的数据变化了，还是在逻辑中主动变动了，都会触发这个事件
+                console.log(newValue);
                 if (value != newValue) {
                     value = newValue;
                     context.changeDom();
@@ -57,7 +57,8 @@ var Compiler = /** @class */ (function () {
     function Compiler(data, methods) {
         this.rootElement = document.querySelector('#ego');
         this.regType = /\{\{[a-z]*\}\}/;
-        this.currentNode = this.rootElement;
+        // 这个当前的遍历节点留下相应的接口，但是目前没有使用，可以做调试用
+        this.currentElement = this.rootElement;
         this.documentFragment = document.createDocumentFragment();
         this.observer = new Observer(data, methods, this);
         // this.observer.data.get()
@@ -65,22 +66,97 @@ var Compiler = /** @class */ (function () {
             // this.observer.data[key].set()
         }
     }
+    // 检测该节点的属性函数
+    Compiler.prototype.isHavingAttr = function (element, attrName) {
+        for (var i = 0; i < element.attributes.length; i++) {
+            if (element.attributes[i].nodeName.indexOf(attrName) > -1) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    };
+    // 检测该节点的监听器的函数
+    Compiler.prototype.isHavingEventFunction = function (element, eventName) {
+        // for (let i = 0;)
+        // getEventListeners是控制台方法，在这里无法调用
+        /*  console.log(getEventListeners(element))
+         let eventObj = getEventListeners(element)
+         if (eventObj[eventName]) {
+           return true
+         } else {
+           return false
+         } */
+    };
     //遍历节点
     Compiler.prototype.traversalElement = function (element) {
-        if (element === void 0) { element = this.documentFragment.childNodes[0]; }
+        if (element === void 0) { element = this.documentFragment.children[0]; }
         var index = 0;
-        this.currentNode = element;
-        while (element.childNodes.length > index + 1) {
-            this.traversalElement(element.childNodes[index]);
-            index++;
+        this.currentElement = element;
+        while (element.children.length > index + 1) {
+            // for循环内使用自己的循环，而不用使用这个循环
+            if (!this.isHavingAttr(element, 'ego-for')) {
+                this.traversalElement(element.children[index]);
+                index++;
+            }
         }
-        // 在这里对于每一个绑定点就实现了defineProperty，这样当发生变化时就可以直接触发回调
-        // 这样只需要在初始化页面的时候调用函数遍历一次就可以了
         this.handleCatchDataEgoModel(element);
         this.handleCatchDataModel(element);
         this.handleCatchEvent(element);
         this.handleCatchIfData(element);
+        this.handleCatchForData(element);
         return;
+    };
+    // 考虑为for循环专门写一个遍历
+    // for循环之中主要需要支持，数据绑定，if判断，以及事件绑定，不支持for循环嵌套
+    Compiler.prototype.traversalForElement = function (element) {
+        // TODO: for循环的专门遍历逻辑
+        var index = 0;
+        while (element.childNodes.length > index + 1) {
+            this.traversalForElement(element.childNodes[index]);
+            index++;
+        }
+        this.handleCatchDataModel(element, true, 'dataList');
+        // this.handleCatchDataEgoModel(element)
+        // this.handleCatchEvent(element)
+        // this.handleCatchIfData(element)
+    };
+    // 判断ego-for
+    Compiler.prototype.handleCatchForData = function (element) {
+        var attributes = element.attributes;
+        if (attributes && attributes.length > 0) {
+            for (var attr in attributes) {
+                if (attr === 'length') {
+                    break;
+                }
+                if (attributes[attr].nodeName.indexOf('ego-for') > -1) {
+                    var dataListName = attributes[attr].value;
+                    var dataList = this.observer.data[dataListName];
+                    if (!this.observer.data[dataListName]) {
+                        this.observer.data[dataListName] = [];
+                    }
+                    var listLength = this.observer.data[dataListName].length;
+                    // 遍历同一个dom，但是要让这个dom渲染listLength次数
+                    var modelElement = document.createDocumentFragment();
+                    modelElement.innerHTML = element.childNodes[0];
+                    for (var i = 0; i < dataList.length; i++) {
+                        console.log(element);
+                        this.traversalForElement(modelElement);
+                        var node = document.createElement("LI");
+                        var textnode = document.createTextNode("Water");
+                        node.appendChild(textnode);
+                        element.appendChild(node);
+                    }
+                    modelElement = null;
+                    // this.traversalElement(element)
+                    // NOTE：这里这个遍历，直接遍历会死循环，是针对这个for节点的内部遍历，他所定义的节点和逻辑是不一样的，所以按道理
+                    // 这里的遍历操作的是for的这个内容，所以我们可以考虑把for的内容当做参数传入traversalElement
+                    // 让基础节点的遍历也可以使用这个参数，然后统一在遍历完之后进行
+                    // this.traversalElement(element)
+                }
+            }
+        }
     };
     // 判断ego-model
     Compiler.prototype.handleCatchDataEgoModel = function (element) {
@@ -90,8 +166,6 @@ var Compiler = /** @class */ (function () {
                 if (this.observer.dataList.indexOf(nodeValue.textContent) === -1) {
                     this.observer.dataList.push(nodeValue.textContent);
                 }
-                // 换成在fragment中操作
-                // NOTE: 这部分内容在切换了数据之后就失去了效用，还需要改良
                 if (this.observer.data[nodeValue.textContent]) {
                     element.innerHTML = this.observer.data[nodeValue.textContent];
                 }
@@ -102,22 +176,25 @@ var Compiler = /** @class */ (function () {
         }
     };
     // 判断{{}}
-    Compiler.prototype.handleCatchDataModel = function (element) {
-        if (element.childNodes.length === 1) {
-            if (this.regType.test(element.childNodes[0].nodeValue)) {
-                element.addEventListener;
-                var nodeValue = this.regType.exec(element.childNodes[0].nodeValue);
-                var result = this.handleMatchingData(nodeValue.input);
-                element.setAttribute('ego-model', result);
-                if (this.observer.dataList.indexOf(result) === -1) {
-                    this.observer.dataList.push(result);
+    Compiler.prototype.handleCatchDataModel = function (element, isForDom, forListName) {
+        // 改写为children而不是使用childNode，因为节点会错误的包括text节点
+        if (element) {
+            if (this.regType.test(element.innerHTML)) {
+                var elementValue = this.regType.exec(element.innerHTML);
+                var result = this.handleMatchingData(elementValue.input);
+                if (isForDom) {
+                    var currentDataList = this.observer.data[forListName];
+                    console.log(currentDataList);
+                    element.innerHTML = currentDataList[0].data;
+                    element.setAttribute('ego-model', currentDataList[0]);
                 }
-                element.innerHTML = this.observer.data[result];
-                // if (this.observer.data[result]) {
-                //   element.innerHTML = this.observer.data[result]
-                // } else {
-                //   element.innerHTML = ''
-                // }
+                else {
+                    if (this.observer.dataList.indexOf(result) === -1) {
+                        this.observer.dataList.push(result);
+                        element.setAttribute('ego-model', result);
+                    }
+                    element.innerHTML = this.observer.data[result];
+                }
             }
         }
     };
@@ -133,9 +210,11 @@ var Compiler = /** @class */ (function () {
                 if (attributes[attr].nodeName.indexOf('ego-bind') > -1) {
                     var eventType = attributes[attr].nodeName.split(':')[1];
                     var eventFunctionName_1 = attributes[attr].textContent;
+                    // NOTE:这里不能使用add，否则会一直添加监听器
                     element.addEventListener(eventType, function () {
                         _this.observer.methods[eventFunctionName_1]();
                     });
+                    var onEventType = 'on' + eventType;
                     return "break";
                 }
             };
@@ -143,24 +222,6 @@ var Compiler = /** @class */ (function () {
                 var state_1 = _loop_1(attr);
                 if (state_1 === "break")
                     break;
-            }
-        }
-    };
-    // 判断ego-for
-    Compiler.prototype.handleCatchForData = function (element) {
-        var attributes = element.attributes;
-        if (attributes && attributes.length > 0) {
-            for (var attr in attributes) {
-                if (attr === 'length') {
-                    break;
-                }
-                if (attributes[attr].nodeName.indexOf('ego-for') > -1) {
-                    var dataListName = attributes[attr].value;
-                    // NOTE：这里这个遍历，是针对这个for节点的内部遍历，他所定义的节点和逻辑是不一样的，所以按道理
-                    // 这里的遍历操作的是for的这个内容，所以我们可以考虑把for的内容当做参数传入traversalElement
-                    // 让基础节点的遍历也可以使用这个参数，然后统一在遍历完之后进行
-                    // this.traversalElement(element)
-                }
             }
         }
     };
@@ -172,7 +233,6 @@ var Compiler = /** @class */ (function () {
                     break;
                 }
                 if (attributes[attr].nodeName.indexOf('ego-if') > -1) {
-                    console.log(attributes[attr].value);
                     if (attributes[attr].value) {
                         var value = attributes[attr].value;
                         // NOTE：先判断非且或，在判断比较符号，在判断运算符
--- egoPolish.ts
+++ egoPolish.ts
@@ -1,8 +1,12 @@
 // 上回书说道
 // 怎么让使用 双括号进行绑定的数据，在渲染一次后，还可以渲染第二次-----修改了绑定的属性的名称，在发现大括号之后，加一层ego-model
 // 新问题，没有办法控制，没使用标签包裹的绑定点(目前使用检测后强制绑定了一个ego-model，但我觉得这样不太科学)
-// 新问题，如果在一个标签内既有{{}}数据绑定，又有
-// 实现ego-if
+// 新问题，如果在一个标签内既有{{}}数据绑定，又有ego-model该如何处理
+// 新问题，事件绑定，在修改数据之后会重复遍历，这样会将数据绑定四次
+
+// ---解决可能性： 给每一个节点添加一个遍历的flag，如果在一次数据的set过程中，遍历过一次了，那就不用在修改其他数据的时候再改变了
+
+// 实现ego-for,整个编译存在疑问，egomodel设置错误
 
 interface Key {
   [key: string]: string
@@ -23,6 +27,9 @@ interface VirtualDom {
   children: VirtualDom[]
 }
 
+
+declare function getEventListeners(element: HTMLElement): any
+
 class Observer {
   public keyObject: VirtualDom = {
     type: 'div',
@@ -41,7 +48,7 @@ class Observer {
     // get和set会将原生的dom操作覆盖掉
     Object.keys(data).forEach(key =>
       // self.defineReactive(data, key, data[key])
-      this.defineppt(data, key, data[key], context)
+      this.defineProp(data, key, data[key], context)
     )
 
   }
@@ -50,10 +57,9 @@ class Observer {
     for (const key in dataChange) {
       this.data[key] = dataChange[key]
     }
-    console.log(this.data)
   }
 
-  private defineppt(data: Key, key: keyof Key, value: any, context: Compiler) {
+  private defineProp(data: Key, key: keyof Key, value: any, context: Compiler) {
     // 注意，在defineProperty之中不能直接使用this.data[key]这样的方式，否则会循环调用set和get函数
     Object.defineProperty(data, key, {
       enumerable: false,
@@ -64,6 +70,7 @@ class Observer {
       },
       set: (newValue) => {
         // 无论是视图上的数据变化了，还是在逻辑中主动变动了，都会触发这个事件
+        console.log(newValue)
         if (value != newValue) {
           value = newValue
           context.changeDom()
@@ -81,7 +88,8 @@ class Compiler {
   public observer!: Observer
   public rootElement: HTMLElement = document.querySelector('#ego') as HTMLElement
   private regType: RegExp = /\{\{[a-z]*\}\}/
-  private currentNode = this.rootElement
+  // 这个当前的遍历节点留下相应的接口，但是目前没有使用，可以做调试用
+  private currentElement = this.rootElement
   public documentFragment: any = document.createDocumentFragment()
 
   constructor(data: Key, methods: Methods) {
@@ -92,22 +100,101 @@ class Compiler {
     }
   }
 
-  //遍历节点
-  public traversalElement(element: HTMLElement = this.documentFragment.childNodes[0]) {
-    let index = 0
-    this.currentNode = element
-    while (element.childNodes.length > index + 1) {
-      this.traversalElement(element.childNodes[index] as HTMLElement)
-      index++
+  // 检测该节点的属性函数
+  private isHavingAttr(element: HTMLElement, attrName: string) {
+    for (let i = 0; i < element.attributes.length; i++) {
+      if (element.attributes[i].nodeName.indexOf(attrName) > -1) {
+        return true
+      } else {
+        return false
+      }
     }
-    // 在这里对于每一个绑定点就实现了defineProperty，这样当发生变化时就可以直接触发回调
-    // 这样只需要在初始化页面的时候调用函数遍历一次就可以了
+  }
+  // 检测该节点的监听器的函数
+  private isHavingEventFunction(element: HTMLElement, eventName: string) {
+    // for (let i = 0;)
+    // getEventListeners是控制台方法，在这里无法调用
+    /*  console.log(getEventListeners(element))
+     let eventObj = getEventListeners(element)
+     if (eventObj[eventName]) {
+       return true
+     } else {
+       return false
+     } */
+  }
+
+  //遍历节点
+  public traversalElement(element: HTMLElement = this.documentFragment.children[0]) {
+    let index = 0
+    this.currentElement = element
+    while (element.children.length > index + 1) {
+      // for循环内使用自己的循环，而不用使用这个循环
+      if (!this.isHavingAttr(element, 'ego-for')) {
+        this.traversalElement(element.children[index] as HTMLElement)
+        index++
+      }
+    }
     this.handleCatchDataEgoModel(element)
     this.handleCatchDataModel(element)
     this.handleCatchEvent(element)
     this.handleCatchIfData(element)
+    this.handleCatchForData(element)
     return
   }
+
+  // 考虑为for循环专门写一个遍历
+  // for循环之中主要需要支持，数据绑定，if判断，以及事件绑定，不支持for循环嵌套
+  public traversalForElement(element: any) {
+    // TODO: for循环的专门遍历逻辑
+    let index = 0
+    while (element.childNodes.length > index + 1) {
+      this.traversalForElement(element.childNodes[index] as HTMLElement)
+      index++
+    }
+    this.handleCatchDataModel(element, true, 'dataList')
+    // this.handleCatchDataEgoModel(element)
+    // this.handleCatchEvent(element)
+    // this.handleCatchIfData(element)
+  }
+
+  // 判断ego-for
+  public handleCatchForData(element: HTMLElement) {
+    const attributes = element.attributes
+    if (attributes && attributes.length > 0) {
+      for (const attr in attributes) {
+        if (attr === 'length') {
+          break
+        }
+        if ((attributes[attr].nodeName as string).indexOf('ego-for') > -1) {
+          let dataListName = attributes[attr].value
+          let dataList = this.observer.data[dataListName]
+          if (!this.observer.data[dataListName]) {
+            (this.observer.data[dataListName] as any) = []
+          }
+          let listLength = this.observer.data[dataListName].length
+          // 遍历同一个dom，但是要让这个dom渲染listLength次数
+          let modelElement: any = document.createDocumentFragment()
+          modelElement.innerHTML = element.childNodes[0]
+          for (let i = 0; i < dataList.length; i++) {
+            console.log(element)
+            this.traversalForElement(modelElement)
+            let node=document.createElement("LI")
+            let textnode=document.createTextNode("Water")
+            node.appendChild(textnode)
+            element.appendChild(node)
+          }
+          modelElement = null
+          // this.traversalElement(element)
+          // NOTE：这里这个遍历，直接遍历会死循环，是针对这个for节点的内部遍历，他所定义的节点和逻辑是不一样的，所以按道理
+          // 这里的遍历操作的是for的这个内容，所以我们可以考虑把for的内容当做参数传入traversalElement
+          // 让基础节点的遍历也可以使用这个参数，然后统一在遍历完之后进行
+          // this.traversalElement(element)
+        }
+      }
+    }
+  }
+
+
   // 判断ego-model
   public handleCatchDataEgoModel(element: HTMLElement) {
     if (element.attributes && element.attributes.length > 0) {
@@ -116,8 +203,6 @@ class Compiler {
         if (this.observer.dataList.indexOf(nodeValue.textContent as string) === -1) {
           this.observer.dataList.push(nodeValue.textContent as string)
         }
-        // 换成在fragment中操作
-        // NOTE: 这部分内容在切换了数据之后就失去了效用，还需要改良
         if (this.observer.data[nodeValue.textContent as string]) {
           element.innerHTML = this.observer.data[nodeValue.textContent as string]
         } else {
@@ -127,22 +212,24 @@ class Compiler {
     }
   }
   // 判断{{}}
-  public handleCatchDataModel(element: HTMLElement) {
-    if (element.childNodes.length === 1) { // 这个判断条件很可能会有问题
-      if (this.regType.test(element.childNodes[0].nodeValue as string)) {
-        element.addEventListener
-        const nodeValue: RegExpExecArray = this.regType.exec(element.childNodes[0].nodeValue as string) as RegExpExecArray
-        const result = this.handleMatchingData(nodeValue.input)
-        element.setAttribute('ego-model', result)
-        if (this.observer.dataList.indexOf(result) === -1) {
-          this.observer.dataList.push(result)
+  public handleCatchDataModel(element: HTMLElement, isForDom?: boolean, forListName?: string) {
+    // 改写为children而不是使用childNode，因为节点会错误的包括text节点
+    if (element) {
+      if (this.regType.test(element.innerHTML)) {
+        const elementValue: RegExpExecArray = this.regType.exec(element.innerHTML as string) as RegExpExecArray
+        const result = this.handleMatchingData(elementValue.input)
+        if (isForDom) {
+          const currentDataList: any = this.observer.data[forListName as string]
+          console.log(currentDataList)
+          element.innerHTML = currentDataList[0].data
+          element.setAttribute('ego-model', currentDataList[0])
+        } else {
+          if (this.observer.dataList.indexOf(result) === -1) {
+            this.observer.dataList.push(result)
+            element.setAttribute('ego-model', result)
+          }
+          element.innerHTML = this.observer.data[result]
         }
-        element.innerHTML = this.observer.data[result]
-        // if (this.observer.data[result]) {
-        //   element.innerHTML = this.observer.data[result]
-        // } else {
-        //   element.innerHTML = ''
-        // }
       }
     }
   }
@@ -157,31 +244,15 @@ class Compiler {
         if ((attributes[attr].nodeName as string).indexOf('ego-bind') > -1) {
           const eventType = attributes[attr].nodeName.split(':')[1]
           const eventFunctionName = attributes[attr].textContent as string
+          // NOTE:这里不能使用add，否则会一直添加监听器
           element.addEventListener(eventType, () => {
             this.observer.methods[eventFunctionName]()
           })
+          let onEventType: any = 'on' + eventType
+          // element[onEventType as string] = () => {
+          //   window.getEventListeners = ()=>void
+          // }
           break
-        }
-      }
-    }
-  }
-
-  // 判断ego-for
-  public handleCatchForData(element: HTMLElement) {
-    const attributes = element.attributes
-    if (attributes && attributes.length > 0) {
-      for (const attr in attributes) {
-        if (attr === 'length') {
-          break
-        }
-        if ((attributes[attr].nodeName as string).indexOf('ego-for') > -1) {
-          let dataListName = attributes[attr].value
-          let dataList = this.observer.data[dataListName]
-
-          // NOTE：这里这个遍历，是针对这个for节点的内部遍历，他所定义的节点和逻辑是不一样的，所以按道理
-          // 这里的遍历操作的是for的这个内容，所以我们可以考虑把for的内容当做参数传入traversalElement
-          // 让基础节点的遍历也可以使用这个参数，然后统一在遍历完之后进行
-          // this.traversalElement(element)
         }
       }
     }
@@ -195,7 +266,6 @@ class Compiler {
           break
         }
         if ((attributes[attr].nodeName as string).indexOf('ego-if') > -1) {
-          console.log(attributes[attr].value)
           if (attributes[attr].value) {
             let value = attributes[attr].value
             // NOTE：先判断非且或，在判断比较符号，在判断运算符
